dc12c47785b95e71352ea32cb6faca13bd04a5e3
Luke <constableldp@gmail.com>
2021-04-17 20:51:33 +0100

    added grid to test

--- client/src/components/Cards.vue
+++ client/src/components/Cards.vue
@@ -1,10 +1,11 @@
 <template>
   <section>
       <h2>Cards</h2>
-
-      <div v-for="(card, index) in this.cards" :key="index">
-        <img class="non-flipped" src="../assets/default.png"> 
-        <img class="flipped" :src="require(`@/assets/imgs/${card.img}`)">
+      <div  class="grid">
+        <div v-for="(card, index) in this.cards" :key="index">
+          <img class="non-flipped" src="../assets/default.png"> 
+          <img class="flipped" :src="require(`@/assets/imgs/${card.img}`)">
+        </div>
       </div>
   </section>
 </template>
@@ -67,9 +68,38 @@ export default {
 
 <style>
 
-img {
-  height: 150px;
-  width: 100px
+.grid {
+  display: grid;
+  grid: repeat(4, 160px) / auto-flow 115px;
+  justify-content: center;
+}
+
+.non-flipped, .flipped {
+  -webkit-backface-visibility: hidden;
+  -moz-backface-visibility: hidden;
+  -o-backface-visibility: hidden;
+  backface-visibility: hidden;
+  -webkit-transition: 0.6s;
+  -webkit-transform-style: preserve-3d;
+  -moz-transition: 0.6s;
+  -moz-transform-style: preserve-3d;
+  -o-transition: 0.6s;
+  -o-transform-style: preserve-3d;
+  -ms-transition: 0.6s;
+  -ms-transform-style: preserve-3d;
+  transition: 0.6s;
+  transform-style: preserve-3d;
+  height: 140px;
+  width: 80px;
+}
+
+.non-flipped {
+  -webkit-transform: rotateY(-180deg);
+  -moz-transform: rotateY(-180deg);
+  -o-transform: rotateY(-180deg);
+  -ms-transform: rotateY(-180deg);
+  transform: rotateY(-180deg);
+  position: absolute;
 }
 
 </style>
