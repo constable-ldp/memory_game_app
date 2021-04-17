01de9bdf3a39c42f8e4bcc14863cf8bd3d3b1cb1
Luke <constableldp@gmail.com>
2021-04-17 20:20:49 +0100

    fixed image issues, and removed database for images

--- client/src/components/Cards.vue
+++ client/src/components/Cards.vue
@@ -3,33 +3,42 @@
       <h2>Cards</h2>
 
       <div v-for="(card, index) in this.cards" :key="index">
-        <img class="non-flipped" src="../assets/default2.png"> 
-        <img class="flipped" :src="card.img">
+        <img class="non-flipped" src="../assets/default.png"> 
+        <img class="flipped" :src="require(`@/assets/imgs/${card.img}`)">
       </div>
   </section>
 </template>
 
 <script>
 
-import CardService from '../services/CardService.js';
+// import CardService from '../services/CardService.js';
 
 export default {
   name: 'cards',
   data() {
     return {
       cards: [],
-      images: []
+      images: [
+        'alex.png', 'dani.png', 'jael.png', 
+        'kamil.png', 'pete.png', 'piotr.png',
+        'stuart.png', 'tim.png'
+        ]
     }
   },
   mounted() {
-    this.fetchImages(),
+
+    // Extention: 
+    // fetchImages will get image from api and replace the data in images.  If greater than 16 chosen or api choosen:
+    // Use a if statment to replace the data in images. And a v-if to change the for loop in the html
+    // this.fetchImages(),
+
     this.assignIds()
   },
   methods: {
-    fetchImages: function(){
-      CardService.getImages()
-      .then(images => this.images = images.img);
-    },
+    // fetchImages: function(){
+    //   CardService.getImages()
+    //   .then(images => this.images = images.img);
+    // },
 
     assignIds: function() {
       for (let i=0; i < this.images.length; i++) {
@@ -50,13 +59,17 @@ export default {
         }
         this.cards.push(cardObj)
       }
-    }
+    },
   },
-
 }
 
 </script>
 
 <style>
+
+img {
+  height: 150px;
+  width: 100px
+}
 
 </style>
