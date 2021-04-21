758ac4bffafa26c66d2c460679760a711aa60937
codeonium <codeon2020@gmail.com>
2021-04-21 16:34:55 +0100

    seeds reborn

---
+++ server/db/seeds.js
@@ -0,0 +1,22 @@
+use memory_game;
+
+db.dropDatabase();
+
+db.players.insertMany([
+    {
+        "name": "Vitor Amandio",
+        "games": []
+      },
+    {
+        "name": "Calum Gunn",
+        "games": []
+      },
+    {
+        "name": "Luke Constable",
+        "games": []
+      },
+    {
+        "name": "Michael McColl",
+        "games": []
+      }
+]);
